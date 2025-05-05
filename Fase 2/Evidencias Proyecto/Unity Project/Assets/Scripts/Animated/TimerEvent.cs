5f0ca995b57e230d5f751ead9f32fdbee9512184
Liân <42004259+laincs@users.noreply.github.com>
2025-05-05 17:22:46 -0400

    Init Project Unity

---
+++ Fase 2/Evidencias Proyecto/Unity Project/Assets/Scripts/Animated/TimerEvent.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TimerEvent : MonoBehaviour
+{
+    [Header("Config")]
+    public int time;
+    public UnityEvent unityEvent;
+    void OnEnable(){
+        
+    }
+}
